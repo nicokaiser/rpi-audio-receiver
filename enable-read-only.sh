1538c56b780a3f8d39c80ab4f7edf764b4a83311
Nico Kaiser <nico.kaiser@boerse-go.de>
2018-02-18 14:59:22 +0100

    read-only: Fix DHCP in read-only script

--- enable-read-only.sh
+++ enable-read-only.sh
@@ -5,6 +5,7 @@
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/read-only-fs.sh
 bash read-only-fs.sh
 
+rm -rf /var/lib/dhcpcd5
 ln -s /tmp /var/lib/dhcpcd5
 systemctl disable apt-daily-upgrade.service
 systemctl disable apt-daily-upgrade.timer
@@ -12,6 +13,7 @@ systemctl disable apt-daily-upgrade.timer
 rm /var/lib/systemd/random-seed
 ln -s /tmp/random-seed /var/lib/systemd/random-seed
 
+mkdir -p /etc/systemd/system/systemd-random-seed.service.d/
 cat <<'EOF' > /etc/systemd/system/systemd-random-seed.service.d/override.conf
 [Service]
 ExecStartPre=/bin/echo "" > /tmp/random-seed

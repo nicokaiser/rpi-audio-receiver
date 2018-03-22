cb5247b77c6b601d4a0fe74b4e70fe0c3779ffef
Nico Kaiser <nico.kaiser@boerse-go.de>
2018-03-22 21:38:24 +0100

    spotify: Bump version

--- install-spotify.sh
+++ install-spotify.sh
@@ -2,9 +2,9 @@
 
 echo "Installing Spotify Connect (spotifyd)"
 
-wget https://github.com/Spotifyd/spotifyd/releases/download/untagged-5714215a4cf45130004f/spotifyd-2018-03-03-armv6.zip
-unzip spotifyd-2018-03-03-armv6.zip
-rm spotifyd-2018-03-03-armv6.zip
+wget https://github.com/Spotifyd/spotifyd/releases/download/untagged-f40914fedc45c74a3412/spotifyd-2018-03-20-armv6.zip
+unzip spotifyd-2018-03-20-armv6.zip
+rm spotifyd-2018-03-20-armv6.zip
 mkdir -p /opt/local/bin
 mv spotifyd /opt/local/bin
 

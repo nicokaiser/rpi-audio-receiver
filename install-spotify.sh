a43bc8ebf3e186a35e3d89eb814c1c652ffc69d2
Torgee <torge.herber@gmail.com>
2018-06-12 18:44:42 +0200

    updating sptofyd link to newest version

--- install-spotify.sh
+++ install-spotify.sh
@@ -2,9 +2,10 @@
 
 echo "Installing Spotify Connect (spotifyd)"
 
-wget https://github.com/Spotifyd/spotifyd/releases/download/untagged-61816928a53a74993dc0/spotifyd-2018-04-03-armv6.zip
-unzip spotifyd-2018-04-03-armv6.zip
-rm spotifyd-2018-04-03-armv6.zip
+wget https://github.com/Spotifyd/spotifyd/releases/download/v0.2.1/spotifyd-2018-05-29-armv6.zip
+unzip spotifyd-2018-05-29-armv6.zip
+rm spotifyd-2018-05-29-armv6.zip
+
 mkdir -p /opt/local/bin
 mv spotifyd /opt/local/bin
 

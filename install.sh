300633ef14c244d534620559e391e2174561a6a8
Nico Kaiser <nico.kaiser@boerse-go.de>
2019-06-26 18:35:44 +0200

    Add snapclient installation to global installation script

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ sudo ./install-bluetooth.sh
 sudo ./install-shairport.sh
 sudo ./install-spotify.sh
 sudo ./install-upnp.sh
+sudo ./install-snapcast-client.sh
 sudo ./install-startup-sound.sh
 sudo ./install-pivumeter.sh
 #sudo ./enable-hifiberry.sh

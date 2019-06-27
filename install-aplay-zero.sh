52678d8fe81b37677c2f76b2cdd53f9f944a6628
Nico Kaiser <nico.kaiser@boerse-go.de>
2019-06-27 08:29:23 +0200

    aplay-zero: Wait for sound to be initialized

--- install-aplay-zero.sh
+++ install-aplay-zero.sh
@@ -7,6 +7,7 @@ if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 1; fi
 sh -c 'cat > /etc/systemd/system/aplay.service' << 'EOL'
 [Unit]
 Description=Invoke aplay from /dev/zero at system start.
+Wants=sound.target
 
 [Service]
 ExecStart=/usr/bin/aplay -D default -t raw -r 44100 -c 2 -f S16_LE /dev/zero

4fbcd2c27d1e4379fe626b6d999d1687342cbd4b
Ahmet Tümiş <56443860+AhmetTumis@users.noreply.github.com>
2019-12-06 23:15:59 +0300

    Custom sound install added

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ CURRENT_PRETTY_HOSTNAME=$(hostnamectl status --pretty)
 read -p "Pretty hostname [${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}]: " PRETTY_HOSTNAME
 sudo hostnamectl set-hostname --pretty "${PRETTY_HOSTNAME:-${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}}"
 
+
+
 echo "Updating packages"
 sudo apt update
 sudo apt upgrade -y
@@ -17,8 +19,15 @@ sudo ./install-shairport.sh
 sudo ./install-spotify.sh
 sudo ./install-upnp.sh
 sudo ./install-snapcast-client.sh
+
+echo -n "Do you want to install a Custom startup sound? [y/N] "
+read REPLY
+if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; 
+then sudo ./install-custom-startup-sound.sh; fi
+
+
 sudo ./install-startup-sound.sh
-sudo ./install-pivumeter.sh
+#sudo ./install-pivumeter.sh
 #sudo ./enable-hifiberry.sh
 sudo ./install-aplay-zero.sh
 sudo ./enable-read-only.sh

1e6d3090e5ac727dc92b49da53d01a378d7d9818
Nico Kaiser <nico@kaiser.me>
2020-01-17 14:35:50 +0100

    Update Spotifyd systemd service

--- install-spotify.sh
+++ install-spotify.sh
@@ -28,20 +28,14 @@ EOF
 cat <<'EOF' > /etc/systemd/system/spotifyd.service
 [Unit]
 Description=Spotify Connect
-Documentation=https://github.com/Spotifyd/spotifyd
-Wants=sound.target
-After=sound.target
 Wants=network-online.target
-After=network-online.target
+After=network.target sound.target
 
 [Service]
-Type=idle
-User=pi
-ExecStart=/usr/local/bin/spotifyd -c /etc/spotifyd.conf --no-daemon
+Type=simple
+ExecStart=/usr/local/bin/spotifyd --config-path /etc/spotifyd.conf --no-daemon
 Restart=always
-RestartSec=10
-StartLimitInterval=30
-StartLimitBurst=20
+RestartSec=5
 
 [Install]
 WantedBy=multi-user.target

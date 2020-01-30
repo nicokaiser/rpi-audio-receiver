27457a223138a58e266a667ea6db095edec02013
Nico Kaiser <nico@kaiser.me>
2020-01-30 11:53:10 +0100

    chore: Move shairport-sync package to files/

--- install-shairport.sh
+++ install-shairport.sh
@@ -5,13 +5,13 @@ read REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 
 apt install --no-install-recommends -y avahi-daemon libavahi-client3 libconfig9 libdaemon0 libjack-jackd2-0 libmosquitto1 libpopt0 libpulse0 libsndfile1 libsoxr0
-dpkg -i shairport-sync_3.3.5-1~bpo10+1_armhf.deb
+dpkg -i files/shairport-sync_3.3.5-1~bpo10+1_armhf.deb
 usermod -a -G gpio shairport-sync
 
 PRETTY_HOSTNAME=$(hostnamectl status --pretty)
 PRETTY_HOSTNAME=${PRETTY_HOSTNAME:-$(hostname)}
 
-cat <<EOF > "${SHAIRPORT_SYSCONFDIR}/shairport-sync.conf"
+cat <<EOF > "/etc/shairport-sync.conf"
 general = {
   name = "${PRETTY_HOSTNAME}";
 }

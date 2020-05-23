9fc3c5926178073c313f11619dbc9b4c333a8199
hosac <hosac@gmx.net>
2020-05-23 18:43:48 +0200

    Add automatic check of architecture in installation script

--- install-spotify.sh
+++ install-spotify.sh
@@ -9,12 +9,22 @@ echo -n "Do you want to install Spotify Connect (spotifyd)? [y/N] "
 read REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 
+# Check architecture (armv6 = Pi Zero W | armv7 = Pi 2/3/4)
+ARCH=$( lscpu | awk '/Architecture:/{print $2}' | sed s/.$// )
+if [ $ARCH == "armv6" ]
+then
+	echo "Using armv6..."
+else
+	echo "Using armv7/armhf..."
+	ARCH="armhf"
+fi
+
 # https://github.com/Spotifyd/spotifyd/releases/download/v0.2.24/spotifyd-linux-${ARCH}-slim.tar.gz
 tar -xvzf files/spotifyd-linux-${ARCH}-slim.tar.gz
 mkdir -p /usr/local/bin
 mv spotifyd /usr/local/bin
 
-
+# Create Spotify config file
 cat <<'EOF' > /etc/spotifyd.conf
 [global]
 backend = alsa
@@ -24,6 +34,7 @@ bitrate = 320
 #zeroconf_port = 4444
 EOF
 
+# Create Spotify service
 cat <<'EOF' > /etc/systemd/system/spotifyd.service
 [Unit]
 Description=A spotify playing daemon
@@ -41,5 +52,7 @@ RestartSec=5
 [Install]
 WantedBy=multi-user.target
 EOF
+
+# Enable and start service
 systemctl daemon-reload
 systemctl enable --now spotifyd.service

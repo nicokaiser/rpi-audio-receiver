e5053b339274053f91e1a2fa258b3201a82cc102
Nico Kaiser <nico@kaiser.me>
2020-07-10 07:34:27 +0200

    fix: remove startup-sounds from install.sh (fixes #53)

--- install.sh
+++ install.sh
@@ -17,7 +17,6 @@ sudo ./install-shairport.sh
 sudo ./install-spotify.sh
 sudo ./install-upnp.sh
 sudo ./install-snapcast-client.sh
-sudo ./install-startup-sound.sh
 sudo ./install-pivumeter.sh
 sudo ./enable-hifiberry.sh
 sudo ./enable-read-only.sh

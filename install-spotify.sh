353a0d462388cceca3357a419d3e5bca05468944
Nico Kaiser <nico@kaiser.me>
2020-09-20 13:20:58 +0200

    Auto-select spotifyd architecture

--- install-spotify.sh
+++ install-spotify.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
 
-ARCH=armhf # Change to armv6 for Raspberry Pi 1/Zero
-
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 echo
 echo -n "Do you want to install Spotify Connect (spotifyd)? [y/N] "
 read REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
+
+ARCH=armhf
+[[ `uname -m` = armv6* ]] && ARCH=armv6 # Raspberry Pi 1/Zero
 
 # https://github.com/Spotifyd/spotifyd/releases/download/v0.2.24/spotifyd-linux-${ARCH}-slim.tar.gz
 tar -xvzf files/spotifyd-linux-${ARCH}-slim.tar.gz

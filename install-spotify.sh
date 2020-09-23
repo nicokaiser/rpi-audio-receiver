553c1ba39ea429dbf8328b90b2b2313b56d1220e
Nico Kaiser <nico@kaiser.me>
2020-09-23 21:37:06 +0200

    Replace spotifyd with raspotify (fixes #65)

--- install-spotify.sh
+++ install-spotify.sh
@@ -3,46 +3,16 @@
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 echo
-echo -n "Do you want to install Spotify Connect (spotifyd)? [y/N] "
+echo -n "Do you want to install Spotify Connect (Raspotify)? [y/N] "
 read REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 
-ARCH=armhf
-[[ `uname -m` = armv6* ]] && ARCH=armv6 # Raspberry Pi 1/Zero
-
-# https://github.com/Spotifyd/spotifyd/releases/download/v0.2.24/spotifyd-linux-${ARCH}-slim.tar.gz
-tar -xvzf files/spotifyd-linux-${ARCH}-slim.tar.gz
-mkdir -p /usr/local/bin
-mv spotifyd /usr/local/bin
+curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
 
 PRETTY_HOSTNAME=$(hostnamectl status --pretty | tr ' ' '-')
 PRETTY_HOSTNAME=${PRETTY_HOSTNAME:-$(hostname)}
 
-cat <<EOF > /etc/spotifyd.conf
-[global]
-backend = alsa
-mixer = Softvol
-volume-control = softvol # alsa
-device_name = ${PRETTY_HOSTNAME}
-bitrate = 320
-#zeroconf_port = 4444
+cat <<EOF > /etc/default/raspotify
+DEVICE_NAME="${PRETTY_HOSTNAME}"
+BITRATE="320"
 EOF
-
-cat <<'EOF' > /etc/systemd/system/spotifyd.service
-[Unit]
-Description=A spotify playing daemon
-Documentation=https://github.com/Spotifyd/spotifyd
-Wants=network-online.target
-After=network.target sound.target
-
-[Service]
-Type=simple
-ExecStart=/usr/local/bin/spotifyd --no-daemon
-Restart=always
-RestartSec=5
-
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl daemon-reload
-systemctl enable --now spotifyd.service

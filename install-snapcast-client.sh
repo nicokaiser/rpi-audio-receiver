5814d72226e6b98a8403a93e4b3592c3451c2aa2
Nico Kaiser <nico@kaiser.me>
2020-10-31 21:35:58 +0100

    fix: snapcast install script returned wrong exit code (fixes #78)

--- install-snapcast-client.sh
+++ install-snapcast-client.sh
@@ -5,6 +5,6 @@ if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 echo
 echo -n "Do you want to install Snapcast client (snapclient})? [y/N] "
 read REPLY
-if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 1; fi
+if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 
 apt install --no-install-recommends -y snapclient

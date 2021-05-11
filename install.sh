b7572f3008493369969a16a651e799a6a06a5642
Flo <floriangapp12@gmail.com>
2021-05-11 11:09:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 if [ "$1" != "-q" ];
 then
 
-read -p "Hostname [$(hostname)]: " HOSTNAME
-sudo raspi-config nonint do_hostname ${HOSTNAME:-$(hostname)}
+  read -p "Hostname [$(hostname)]: " HOSTNAME
+  sudo raspi-config nonint do_hostname ${HOSTNAME:-$(hostname)}
 
-CURRENT_PRETTY_HOSTNAME=$(hostnamectl status --pretty)
-read -p "Pretty hostname [${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}]: " PRETTY_HOSTNAME
-sudo hostnamectl set-hostname --pretty "${PRETTY_HOSTNAME:-${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}}"
+  CURRENT_PRETTY_HOSTNAME=$(hostnamectl status --pretty)
+  read -p "Pretty hostname [${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}]: " PRETTY_HOSTNAME
+  sudo hostnamectl set-hostname --pretty "${PRETTY_HOSTNAME:-${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}}"
 
 fi
 
@@ -17,11 +17,11 @@ sudo apt update
 sudo apt upgrade -y
 
 echo "Installing components"
-sudo ./install-bluetooth.sh
-sudo ./install-shairport.sh
-sudo ./install-spotify.sh
-sudo ./install-upnp.sh
-sudo ./install-snapcast-client.sh
-sudo ./install-pivumeter.sh
-sudo ./enable-hifiberry.sh
-sudo ./enable-read-only.sh
+if [[ $FEATURES = *bluetooth* ]]; then sudo ./install-bluetooth.sh $@; fi
+if [[ $FEATURES = *shairport* ]]; then sudo ./install-shairport.sh $@; fi
+if [[ $FEATURES = *spotify* ]]; then sudo ./install-spotify.sh $@; fi
+if [[ $FEATURES = *upnp* ]]; then sudo ./install-upnp.sh $@; fi
+if [[ $FEATURES = *snapcast* ]]; then sudo ./install-snapcast-client.sh $@; fi
+if [[ $FEATURES = *pivumeter* ]]; then sudo ./install-pivumeter.sh $@; fi
+if [[ $FEATURES = *hifiberry* ]]; then sudo ./enable-hifiberry.sh $@; fi
+if [[ $FEATURES = *read-only* ]]; then sudo ./enable-read-only.sh $@; fi

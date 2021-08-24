896d9b366e4b64ef1bb4011296310fc75a2065b6
Andre Reigoto <andrereigoto@gmail.com>
2021-08-24 18:55:59 +0100

    update from master fork

--- install-shairport.sh
+++ install-shairport.sh
@@ -7,17 +7,44 @@ echo -n "Do you want to install Shairport Sync AirPlay Audio Receiver (shairport
 read REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 
-#apt install --no-install-recommends -y autoconf automake avahi-daemon build-essential git libasound2-dev libavahi-client-dev libconfig-dev libdaemon-dev libpopt-dev libssl-dev libtool xmltoman pkg-config
-apt install --no-install-recommends -y avahi-daemon build-essential git xmltoman autoconf automake libtool libdaemon-dev libpopt-dev libconfig-dev libasound2-dev libpulse-dev  libavahi-client-dev libssl-dev libsoxr-dev
+# apt install --no-install-recommends -y autoconf automake avahi-daemon build-essential libasound2-dev libavahi-client-dev libconfig-dev libdaemon-dev libpopt-dev libssl-dev libtool xmltoman pkg-config git
+# apt install --no-install-recommends -y autoconf automake avahi-daemon build-essential libasound2-dev libavahi-client-dev libconfig-dev libdaemon-dev libpopt-dev libssl-dev libtool xmltoman libpulse-dev libsoxr-dev git
+apt install --no-install-recommends -y autoconf automake avahi-daemon build-essential libasound2-dev libavahi-client-dev libconfig-dev libdaemon-dev libpopt-dev libssl-dev libtool xmltoman pkg-config libsoxr0 libsoxr-dev libsndfile1 libsndfile1-dev libglib2.0-dev libmosquitto-dev libmosquitto1
 
+# Install ALAC from GitHub
+wget -O alac.tar.gz https://github.com/TimothyGu/alac/archive/refs/heads/master.tar.gz
+tar xzf alac.tar.gz
+rm alac.tar.gz
+cd alac-master
+autoreconf -fi
+./configure
+make -j $(nproc)
+make install
+cd ..
+rm -rf alac-master
+ldconfig
 
+# Install shairport-sync from GitHub
+# Good version 3.3.7
 git clone https://github.com/mikebrady/shairport-sync.git
 cd shairport-sync 
 last_stable_tag=$(git tag | grep -oP "^[0-9.]+$" | tail -1)
 git checkout $last_stable_tag
 autoreconf -fi
-./configure --sysconfdir=/etc --with-alsa --with-avahi --with-ssl=openssl --with-systemd --with-metadata
-make
+./configure \
+    --with-alsa \
+    --with-dummy \
+    --with-avahi \
+    --with-ssl=openssl \
+    --with-soxr \
+    --with-systemd \
+    --sysconfdir=/etc \
+    --with-dbus-interface \
+    --with-mpris-interface \
+    --with-mqtt-client \
+    --with-apple-alac \
+    --with-convolution
+make -j $(nproc)
 make install
 cd ..
 

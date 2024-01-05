9ed8fdb9c54f4a6ac20c4e6508f0b9492da1c1bc
Nico Kaiser <nico@kaiser.me>
2024-01-05 16:30:12 +0100

    feat: move everything into one script (fixes #166)

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Devices like phones, tablets and computers can play audio via this receiver.
 ## Requirements
 
 - A USB Bluetooth dongle (the internal Raspberry Pi Bluetooth chipset turned out as not suited for audio playback and causes all kinds of strange connectivity problems)
-- Raspberry Pi OS 12 Lite (64-bit)
+- Raspberry Pi OS 12 Lite
 - Internal audio, HDMI, USB or I2S Audio adapter (tested with [Adafruit USB Audio Adapter](https://www.adafruit.com/product/1475),  [pHAT DAC](https://shop.pimoroni.de/products/phat-dac), and [HifiBerry DAC+](https://www.hifiberry.com/products/dacplus/))
 
 **Again: do not try to use the internal Bluetooth chip, this will only bring you many hours of frustration.**
@@ -35,15 +35,6 @@ Lets you choose the hostname and the visible device name ("pretty hostname") whi
 
 Sets up Bluetooth, adds a simple agent that accepts every connection, and enables audio playback through ALSA. A udev script is installed that disables discoverability while connected.
 
-### Bluetooth Configuration
-
-    sudo bluetoothctl
-    power on
-    agent on
-    # Now search for available bluetooth devices from your device
-    # Once paired note down the MAC address of your device 
-    trust 00:00:00:00:00:00 # Put device MAC address here so after reboot it can automatically re-connect again
-
 ### AirPlay 2
 
 Installs [Shairport Sync](https://github.com/mikebrady/shairport-sync) AirPlay 2 Audio Receiver.
@@ -52,9 +43,90 @@ Installs [Shairport Sync](https://github.com/mikebrady/shairport-sync) AirPlay 2
 
 Installs [Raspotify](https://github.com/dtcooper/raspotify), an open source Spotify client for Raspberry Pi.
 
+## Optional steps
+
 ### Read-only mode
 
-To avoid SD card corruption when powering off, you can boot Raspberry Pi OS in read-only mode. This can be achieved using the `raspi-config` script.
+To avoid SD card corruption when powering off, you can boot Raspberry Pi OS in read-only mode. This can be achieved using the `raspi-config` script (in the "Performance" section).
+
+### Add Bluetooth devices
+
+The device should be visible for new Bluetooth connections, but in some cases you might need to pair them manually:
+
+    sudo bluetoothctl
+    power on
+    agent on
+    # Now search for available bluetooth devices from your device
+    # Once paired note down the MAC address of your device 
+    trust 00:00:00:00:00:00 # Put device MAC address here so after reboot it can automatically re-connect again
+
+### Disable Wi-Fi when Bluetooth is connected
+
+Dislaimer: You really might want to use a Bluetooth USB dongle. The internal Bluetooth module of the Raspberry Pi (all versions) is very limited and using it for audio transmission will most certainly lead to problems and unexpected behaviour, see [raspberrypi/linux/#1402](https://github.com/raspberrypi/linux/issues/1402).
+
+If you really want to use the internal Bluetooth module, you almost certainly need to disable Wi-Fi.
+
+Modify `/usr/local/bin/bluetooth-udev` and remove the comments (`#`) around the `ifconfig` calls:
+
+```
+...
+if [ "$action" = "add" ]; then
+    ...
+    # disconnect wifi to prevent dropouts
+    ifconfig wlan0 down &
+fi
+
+if [ "$action" = "remove" ]; then
+    # reenable wifi
+    ifconfig wlan0 up &
+    ...
+fi
+```
+
+### Bluetooth A2DP volume
+
+To enable A2DP volume control, add the `--plugin=a2dp` parameter to the `bluetoothd` command line. This helps setting the volume via Bluetooth, but does not work on all setups.
+
+```sh
+# Enable A2DP volume control
+mkdir -p /etc/systemd/system/bluetooth.service.d
+cat <<'EOF' > /etc/systemd/system/bluetooth.service.d/override.conf
+[Service]
+ExecStart=
+ExecStart=/usr/libexec/bluetooth/bluetoothd --plugin=a2dp
+EOF
+```
+
+### Bluetooth Fast Connectable
+
+Using the `FastConnectable` flag may lead to faster Bluetooth connections, but may also lead to poor sound quality. You can try and see if it works for you. See [#70](https://github.com/nicokaiser/rpi-audio-receiver/issues/70)
+
+Add the flag to the `General` section in `/etc/bluetooth/main.conf`:
+
+```
+[General]
+...
+FastConnectable = true
+...
+```
+
+### Bluetooth pairing with PIN
+
+To enable pairing with a PIN code instead of Simple Secure Pairing mode, the following steps are required:
+
+1. Change `sspmode 1` to `sspmode 0` in `/etc/systemd/system/bt-agent@.service`
+2. Add ` --pin /etc/bluetooth/pin.conf` to the `ExecStart` line in `/etc/systemd/system/bt-agent@.service`
+3. Add a file `/etc/bluetooth/pin.conf` which contains PIN code for the devices:
+
+`AA:BB:CC:DD:EE:FF 1234` (replace `AA:BB:CC:DD:EE:FF` with your Bluetooth devices's Mac address or `*` to use PIN `1234` for all devices)
+
+#### Notes
+
+- **This does not work with iOS devices.** iOS refuses to pair with devices that do not support SSP. So sspmode needs to be on
+- Even with sspmode=1 and a PIN file, iOS would not connect at all
+- macOS allows pairing with devices with sspmode=0 (and a PIN configured on the Pi). When sspmode=1, macOS decides upon a PIN and either the Pi has a fixed PIN list (which does not match => connection refused), or requires keyboard input on the Pi (which is not desired for a headless device).
+
+So you need to try yourself if this works with your setup.
 
 ## Limitations
 
@@ -63,10 +135,6 @@ To avoid SD card corruption when powering off, you can boot Raspberry Pi OS in r
 - To permanently save paired devices when using read-only mode, the Raspberry has to be switched to read-write mode until all devices have been paired once.
 - You might want to use a Bluetooth USB dongle or have the script disable Wi-Fi while connected (see `bluetooth-udev`), as the BCM43438 (Raspberry Pi 3, Zero W) has severe problems with both switched on, see [raspberrypi/linux/#1402](https://github.com/raspberrypi/linux/issues/1402).
 - The Pi Zero may not be powerful enough to play 192 kHz audio, you may want to change the values in `/etc/asound.conf` accordingly.
-
-## Wiki
-
-There are some further examples, tweaks and how-tos in the [GitHub Wiki](https://github.com/nicokaiser/rpi-audio-receiver/wiki).
 
 ## Disclaimer
 
--- install.sh
+++ install.sh
@@ -1,18 +1,208 @@
-#!/bin/bash -e
+#!/bin/bash
 
-read -p "Hostname [$(hostname)]: " HOSTNAME
-sudo raspi-config nonint do_hostname ${HOSTNAME:-$(hostname)}
+set -e
 
-CURRENT_PRETTY_HOSTNAME=$(hostnamectl status --pretty)
-read -p "Pretty hostname [${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}]: " PRETTY_HOSTNAME
-sudo hostnamectl set-hostname --pretty "${PRETTY_HOSTNAME:-${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}}"
+NQPTP_VERSION="1.2.4"
+SHAIRPORT_SYNC_VERSION="4.3.2"
+TMP_DIR=""
 
-echo "Updating packages"
-sudo apt update
-sudo apt upgrade -y
+cleanup() {
+    if [ -d "${TMP_DIR}" ]; then
+        rm -rf "${TMP_DIR}"
+    fi
+}
 
-echo "Installing components"
-./install-bluetooth.sh
-./install-shairport.sh
-./install-spotify.sh
-./enable-hifiberry.sh
+verify_os() {
+    MSG="Unsupported OS: Raspberry Pi OS 12 (bookworm) is required."
+
+    if [ ! -f /etc/os-release ]; then
+        echo $MSG
+        exit 1
+    fi
+
+    . /etc/os-release
+
+    if [ "$ID" != "debian" ] || [ "$VERSION_ID" != "12" ]; then
+        echo $MSG
+        exit 1
+    fi
+}
+
+set_hostname() {
+    CURRENT_PRETTY_HOSTNAME=$(hostnamectl status --pretty)
+
+    read -p "Hostname [$(hostname)]: " HOSTNAME
+    sudo raspi-config nonint do_hostname ${HOSTNAME:-$(hostname)}
+
+    read -p "Pretty hostname [${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}]: " PRETTY_HOSTNAME
+    PRETTY_HOSTNAME="${PRETTY_HOSTNAME:-${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}}"
+    sudo hostnamectl set-hostname --pretty "$PRETTY_HOSTNAME"
+}
+
+install_bluetooth() {
+    read -p "Do you want to install Bluetooth Audio (ALSA)? [y/N] " REPLY
+    if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then return; fi
+
+    # Bluetooth Audio ALSA Backend (bluez-alsa-utils)
+    sudo apt update
+    sudo apt install -y --no-install-recommends bluez-tools bluez-alsa-utils
+
+    # Bluetooth settings
+    sudo tee /etc/bluetooth/main.conf >/dev/null <<'EOF'
+[General]
+Class = 0x200414
+DiscoverableTimeout = 0
+
+[Policy]
+AutoEnable=true
+EOF
+
+    # Bluetooth Agent
+    sudo tee /etc/systemd/system/bt-agent@.service >/dev/null <<'EOF'
+[Unit]
+Description=Bluetooth Agent
+Requires=bluetooth.service
+After=bluetooth.service
+
+[Service]
+ExecStartPre=/usr/bin/bluetoothctl discoverable on
+ExecStartPre=/bin/hciconfig %I piscan
+ExecStartPre=/bin/hciconfig %I sspmode 1
+ExecStart=/usr/bin/bt-agent --capability=NoInputNoOutput
+RestartSec=5
+Restart=always
+KillSignal=SIGUSR1
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    sudo systemctl daemon-reload
+    sudo systemctl enable bt-agent@hci0.service
+
+    # Bluetooth udev script
+    sudo tee /usr/local/bin/bluetooth-udev >/dev/null <<'EOF'
+#!/bin/bash
+if [[ ! $NAME =~ ^\"([0-9A-F]{2}[:-]){5}([0-9A-F]{2})\"$ ]]; then exit 0; fi
+
+action=$(expr "$ACTION" : "\([a-zA-Z]\+\).*")
+
+if [ "$action" = "add" ]; then
+    bluetoothctl discoverable off
+    # disconnect wifi to prevent dropouts
+    #ifconfig wlan0 down &
+fi
+
+if [ "$action" = "remove" ]; then
+    # reenable wifi
+    #ifconfig wlan0 up &
+    bluetoothctl discoverable on
+fi
+EOF
+    sudo chmod 755 /usr/local/bin/bluetooth-udev
+
+    sudo tee /etc/udev/rules.d/99-bluetooth-udev.rules >/dev/null <<'EOF'
+SUBSYSTEM=="input", GROUP="input", MODE="0660"
+KERNEL=="input[0-9]*", RUN+="/usr/local/bin/bluetooth-udev"
+EOF
+}
+
+install_shairport() {
+    read -p "Do you want to install Shairport Sync (AirPlay 2 audio player)? [y/N] " REPLY
+    if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then return; fi
+
+    sudo apt update
+    sudo apt install -y --no-install-recommends wget unzip autoconf automake build-essential libtool git autoconf automake libpopt-dev libconfig-dev libasound2-dev avahi-daemon libavahi-client-dev libssl-dev libsoxr-dev libplist-dev libsodium-dev libavutil-dev libavcodec-dev libavformat-dev uuid-dev libgcrypt20-dev xxd
+
+    if [[ -z "$TMP_DIR" ]]; then
+        TMP_DIR=$(mktemp -d)
+    fi
+
+    cd $TMP_DIR
+
+    # Install ALAC
+    wget -O alac-master.zip https://github.com/mikebrady/alac/archive/refs/heads/master.zip
+    unzip alac-master.zip
+    cd alac-master
+    autoreconf -fi
+    ./configure
+    make -j $(nproc)
+    sudo make install
+    sudo ldconfig
+    cd ..
+    rm -rf alac-master
+
+    # Install NQPTP
+    wget -O nqptp-${NQPTP_VERSION}.zip https://github.com/mikebrady/nqptp/archive/refs/tags/${NQPTP_VERSION}.zip
+    unzip nqptp-${NQPTP_VERSION}.zip
+    cd nqptp-${NQPTP_VERSION}
+    autoreconf -fi
+    ./configure --with-systemd-startup
+    make -j $(nproc)
+    sudo make install
+    cd ..
+    rm -rf nqptp-${NQPTP_VERSION}
+
+    # Install Shairport Sync
+    wget -O shairport-sync-${SHAIRPORT_SYNC_VERSION}.zip https://github.com/mikebrady/shairport-sync/archive/refs/tags/${SHAIRPORT_SYNC_VERSION}.zip
+    unzip shairport-sync-${SHAIRPORT_SYNC_VERSION}.zip
+    cd shairport-sync-${SHAIRPORT_SYNC_VERSION}
+    autoreconf -fi
+    ./configure --sysconfdir=/etc --with-alsa --with-soxr --with-avahi --with-ssl=openssl --with-systemd --with-airplay-2 --with-apple-alac
+    make -j $(nproc)
+    sudo make install
+    cd ..
+    rm -rf shairport-sync-${SHAIRPORT_SYNC_VERSION}
+
+    # Configure Shairport Sync
+    sudo tee /etc/shairport-sync.conf >/dev/null <<'EOF'
+general = {
+  name = "${PRETTY_HOSTNAME:-$(hostname)}";
+  output_backend = "alsa";
+}
+
+sessioncontrol = {
+  session_timeout = 20;
+};
+EOF
+
+    sudo usermod -a -G gpio shairport-sync
+    sudo systemctl enable --now nqptp
+    sudo systemctl enable --now shairport-sync
+}
+
+install_raspotify() {
+    read -p "Do you want to install Raspotify (Spotify Connect)? [y/N] " REPLY
+    if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then return; fi
+
+    # Install Raspotify
+    curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
+
+    # Configure Raspotify
+    LIBRESPOT_NAME="${PRETTY_HOSTNAME// /-}"
+    LIBRESPOT_NAME=${LIBRESPOT_NAME:-$(hostname)}
+
+    sudo tee /etc/raspotify/conf >/dev/null <<'EOF'
+LIBRESPOT_QUIET=
+LIBRESPOT_AUTOPLAY=
+LIBRESPOT_DISABLE_AUDIO_CACHE=
+LIBRESPOT_DISABLE_CREDENTIAL_CACHE=
+LIBRESPOT_ENABLE_VOLUME_NORMALISATION=
+LIBRESPOT_NAME="${LIBRESPOT_NAME}"
+LIBRESPOT_DEVICE_TYPE="avr"
+LIBRESPOT_BITRATE="320"
+LIBRESPOT_INITIAL_VOLUME="100"
+EOF
+
+    sudo systemctl daemon-reload
+    sudo systemctl enable raspotify
+}
+
+trap cleanup EXIT
+
+echo "Raspberry Pi Audio Receiver"
+
+verify_os
+set_hostname
+install_bluetooth
+install_shairport
+install_raspotify
